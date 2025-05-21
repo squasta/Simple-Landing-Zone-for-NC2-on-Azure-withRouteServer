41868034b0994cc871d2fe0b62b593e3d10ea0f3
Stanislas Quastana <squastana@gmail.com>
2025-05-21 08:57:37 +0200

    few changes

--- RouteServer.tf
+++ RouteServer.tf
@@ -87,3 +87,24 @@ resource "azurerm_virtual_network_peering" "PCVNet_to_RouteServerVNet" {
     allow_gateway_transit     = false
     use_remote_gateways       = false
 }
+
+# resource "azurerm_virtual_network_peering" "ClusterVNet_to_RouteServerVNet" {
+#   name                      = "ClusterVNet-to-RouteServerVNet"
+#   resource_group_name       = azurerm_resource_group.TF_RG.name
+#   virtual_network_name      = azurerm_virtual_network.TF_Cluster_VNet.name
+#   remote_virtual_network_id = azurerm_virtual_network.TF_RouteServer_VNet.id
+#   allow_forwarded_traffic   = true
+#   allow_gateway_transit     = false
+#   use_remote_gateways       = false
+# }
+
+# resource "azurerm_virtual_network_peering" "RouteServerVNet_to_ClusterVNet" {
+#   name                      = "RouteServerVNet-to-ClusterVNet"
+#   resource_group_name       = azurerm_resource_group.TF_RG.name
+#   virtual_network_name      = azurerm_virtual_network.TF_RouteServer_VNet.name
+#   remote_virtual_network_id = azurerm_virtual_network.TF_Cluster_VNet.id
+#   allow_forwarded_traffic   = true
+#   allow_gateway_transit     = false
+#   use_remote_gateways       = false
+# }
+
